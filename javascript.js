0067104879c9aa371015a1c655f67e0e97429e63
ludel15 <lucia_roci@hotmail.com>
2023-09-02 23:47:49 -0300

    fix script

--- javascript.js
+++ javascript.js
@@ -7,21 +7,25 @@ function correr(){
     let apellido = prompt ("Por Favor Ingrese su Apellido");
     let nombre = prompt ("Ahora Ingrese su Nombre");
     let edad = prompt ("Cual es tu edad?");
-    let año_actual = prompt ("2023");
-    let jubilacion = prompt ("60");
+    let año_actual =  2023;
+    let jubilacion =  60;
 
     let calculo = + jubilacion - + edad;
     let jubilo = + año_actual + calculo;
 
-    alert ("Hola " + nombre + "" + apellido );
-    alert ("Te vas a jubilar en el año" + jubilo);
+    alert ("Hola " + nombre + ", " + apellido + "! te vas a jubilar en el año " +  jubilo);
 }
 
-
+function calcular (){
     var numero1 = parseInt(prompt("Ingrese el numero que sera sumado en cada interaccion"));
     var suma = 0;
-    for(let i = 0; i < 10; i++) {
-    var numero2 = parseInt(prompt("Ingrese el numero para sumar"));
-    suma = numero1 + numero2;
+    var numero2 = parseInt(prompt("Ingrese el numero de iteraciones"));
+    console.log(numero1, numero2)
+    for(let i = numero1; i <= numero2; i++) {
+    suma = suma + numero1;
     console.log(suma);
     }
+    alert ("El resultado es:  " +  suma);
+
+}
+    
